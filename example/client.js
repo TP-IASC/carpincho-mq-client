726f0f26b2a876c9bd605a613d3a316d3315ee32
CimimUxMaio <cimimuxmaio@gmail.com>
2021-12-04 17:52:33 -0300

    sender and receiver sockets

--- example/client.js
+++ example/client.js
@@ -1,12 +1,18 @@
 import carpinchoMQ, { Transactional } from "../lib/index";
 
+const server = {
+    address: "localhost",
+    port: 8080
+}
 
-const client = carpinchoMQ("localhost:8080");
+const client = carpinchoMQ(server, 41234);
 
 client.setMode(new Transactional());
+
+client.subscribe("cola1");
 
 client.on("cola1", (msg) => {
     console.log(msg);
 });
 
-client.listen(41234);
+client.listen();
