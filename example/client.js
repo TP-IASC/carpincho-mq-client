fa466df614ddd43acb9fcfc6e8835c6c9cf126e2
CimimUxMaio <cimimuxmaio@gmail.com>
2021-12-08 02:09:00 -0300

    Transacion modes removed

--- example/client.js
+++ example/client.js
@@ -1,4 +1,4 @@
-import carpinchoMQ, { Transactional } from "../lib/index";
+import carpi from "../lib/index";
 
 const server = {
     address: "localhost",
@@ -8,13 +8,12 @@ const server = {
 
 const port = Number(process.argv[2]);
 
-const client = carpinchoMQ(server, port);
-
-client.setMode(new Transactional());
+const client = carpi(server, port);
 
 client.subscribe("cola1");
 client.subscribe("cola2");
 client.subscribe("cola3");
+
 
 client.on("cola3", (msg) => {
     for(let i=0; i < 99999; i++) { console.log(i); }
